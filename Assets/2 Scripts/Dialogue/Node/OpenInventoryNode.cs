ae3d2905153c9d85cf9c9174b7e2ca10ebf775bc
Noah99099 <154212329+Noah99099@users.noreply.github.com>
2025-11-22 02:57:52 +0800

    20251122 對話系統新功能上線

--- Assets/2 Scripts/Dialogue/Node/OpenInventoryNode.cs
+++ Assets/2 Scripts/Dialogue/Node/OpenInventoryNode.cs
@@ -4,7 +4,11 @@ using XNode;
 [NodeTint(0.2f, 0.6f, 0.6f)]
 public class OpenInventoryNode : BaseNode
 {
-    [Input(connectionType = ConnectionType.Override)] public BaseNode entry;
+    // --- 【核心修正】 ---
+    // 將 ConnectionType.Override 改為 ConnectionType.Multiple
+    // 這樣它就可以同時接收 "Start節點的線" 和 "迴圈回來的線"
+    [Input(connectionType = ConnectionType.Multiple)] public BaseNode entry; 
+    // -------------------
     [Output(connectionType = ConnectionType.Override)] public BaseNode exit;
 
     protected override void Init()
